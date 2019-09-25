bf75ad1f3642df5fae96260defa232cf808ca0a8
Laurent RENARD <lorenzofox@Laurents-MacBook-Pro.local>
2019-09-25 17:26:01 -0400

    try fix ci

--- test/run-screenshots.js
+++ test/run-screenshots.js
@@ -18,24 +18,29 @@ const createScreenShot = async name => {
 };
 
 test('screen shots', async t => {
-    const cases = (await globby('./test/screenshots/cases/*.html'))
-        .map(f => f.split('/')[4])
-        .map(f => f.split('.')[0]);
-    const caseNames = cases.map(fileName => fileName.split('.')[0]);
+    try {
+        const cases = (await globby('./test/screenshots/cases/*.html'))
+            .map(f => f.split('/')[4])
+            .map(f => f.split('.')[0]);
+        const caseNames = cases.map(fileName => fileName.split('.')[0]);
 
-    await t.test('taking screenshots', async t => {
-        return Promise.all(caseNames.map(name => t.test(`taking screen shot for ${name}`, async t => {
-            await createScreenShot(name);
-            t.ok(true, `screen shot for ${name} successfully taken`);
-        })));
-    });
+        await t.test('taking screenshots', async t => {
+            return Promise.all(caseNames.map(name => t.test(`taking screen shot for ${name}`, async t => {
+                await createScreenShot(name);
+                t.ok(true, `screen shot for ${name} successfully taken`);
+            })));
+        });
 
-    await t.test('comparing screenshots with standards', async t => {
-        return Promise.all(caseNames.map(name => t.test(`comparing for "${name}" case`, t => {
-            const current = PNG.sync.read(readFileSync(resolve(process.cwd(), `./test/dist/screenshots/${name}.png`)));
-            const standard = PNG.sync.read(readFileSync(resolve(process.cwd(), `./test/screenshots/standards/${name}.png`)));
-            const diffCount = pixelmatch(current.data, standard.data, null, 800, 600, {threshold: 0.1});
-            t.eq(diffCount, 0, 'pixel diff count should be 0');
-        })));
-    });
+        await t.test('comparing screenshots with standards', async t => {
+            return Promise.all(caseNames.map(name => t.test(`comparing for "${name}" case`, t => {
+                const current = PNG.sync.read(readFileSync(resolve(process.cwd(), `./test/dist/screenshots/${name}.png`)));
+                const standard = PNG.sync.read(readFileSync(resolve(process.cwd(), `./test/screenshots/standards/${name}.png`)));
+                const diffCount = pixelmatch(current.data, standard.data, null, 800, 600, {threshold: 0.1});
+                t.eq(diffCount, 0, 'pixel diff count should be 0');
+            })));
+        });
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
 });
--- test/run-unit.js
+++ test/run-unit.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-    });
-    const page = await browser.newPage();
-    page.on('console', ev => console.log(ev.text()));
-    await page.goto(`http://localhost:3002/test/dist/index.html`);
-    await browser.close();
+    try {
+        const browser = await puppeteer.launch({
+            executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        });
+        const page = await browser.newPage();
+        page.on('console', ev => console.log(ev.text()));
+        await page.goto(`http://localhost:3002/test/dist/index.html`);
+        await browser.close();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
 })();
